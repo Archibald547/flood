b32d38be7c57f4438f9dc05b31b27e99dcbfecf0
Jenny <nzhe4831@uni.sydney.edu.au>
2017-09-18 16:19:57 +1000

    startapp, create template folders

--- floodSite/floodSite/wsgi.py
+++ floodSite/floodSite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for flood project.
+WSGI config for floodSite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flood.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "floodSite.settings")
 
 application = get_wsgi_application()
