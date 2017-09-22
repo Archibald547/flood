88547261de272df2be59c682e5eadf8b0987a417
Jenny <nzhe4831@uni.sydney.edu.au>
2017-09-22 13:25:43 +1000

    template for register. not modified. used bootstrap

--- floodSite/floodApp/views.py
+++ floodSite/floodApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home/main.html')
+
+def rego(request):
+    return render(request,'home/register.html')
