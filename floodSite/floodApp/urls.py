e6da1a52931c09f4fb4dd4006502b1fc641ea954
Jenny <nzhe4831@uni.sydney.edu.au>
2017-10-06 12:52:16 +1100

    created login dropdown.TODO: redirect to user page once logged in

--- floodSite/floodApp/urls.py
+++ floodSite/floodApp/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from . import views
+from django.contrib.auth.views import login, logout
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
     url(r'^register/$',views.rego, name="register"),
     url(r'^dashboard/$', views.dashboard, name="dashboard"),
     url(r'^profile/$', views.profile, name="profile"),
-    url(r'', views.home, name="home"),
+    url(r'home/$', views.home, name="home"),
+    url(r'^login/$', login, {'template_name':'home/template/base.html'}, name="login"),
+    url(r'^logout/$',logout, {'next_page':reverse_lazy('home')}, name="logout")
 ]
