1ab3d1100f8ece706822b3e185b6cc855166e661
Jenny <nzhe4831@uni.sydney.edu.au>
2017-10-06 17:23:39 +1100

    change urls in floodsite to access viewsite from admin

--- floodSite/floodApp/urls.py
+++ floodSite/floodApp/urls.py
@@ -4,10 +4,11 @@ from django.contrib.auth.views import login, logout
 from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
+    url(r'^$', views.home, name="home"),
     url(r'^register/$',views.rego, name="register"),
     url(r'^dashboard/$', views.dashboard, name="dashboard"),
     url(r'^profile/$', views.profile, name="profile"),
-    url(r'home/$', views.home, name="home"),
-    url(r'^login/$', login, {'template_name':'home/template/base.html'}, name="login"),
+    # url(r'home/$', views.home, name="home"),
+    url(r'^login/$', login, {'template_name':'home/template/main.html'}, name="login"),
     url(r'^logout/$',logout, {'next_page':reverse_lazy('home')}, name="logout")
 ]
--- floodSite/floodSite/urls.py
+++ floodSite/floodSite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^floodApp/', include('floodApp.urls')),
+    url(r'', include('floodApp.urls')),
 ]
