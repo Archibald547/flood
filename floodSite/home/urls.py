932a92544a9d68231205ea432c35e1561993e1a3
Jenny <nzhe4831@uni.sydney.edu.au>
2017-10-10 04:02:06 +1100

    TODO: redo login using package DONE: tidy up templates and create share templates , staticto be shared with other apps. modified signup please install crispy form setup static in settings navbar is fixed when scrolling up and down

--- floodSite/home/urls.py
+++ floodSite/home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from home import views
 
+
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('registration.backends.hmac.urls'), name="register"),
     url(r'^privacy_policy/$', views.privacy_policy.as_view(), name="privacy_policy"),
 ]
--- floodSite/home/views.py
+++ floodSite/home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 # from floodApp.forms import UserCreationForm
 # from django.contrib.auth.models import User
 # from django.http import JsonResponse
@@ -13,3 +16,18 @@ def home(request):
 
 class privacy_policy(TemplateView):
     template_name = "privacy_policy.html"
+
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return HttpResponseRedirect("dashboard.html")
+        else:
+            messages.info(request, 'Disabled account')
+
+    else:
+            messages.info(request, 'Invalid login')
