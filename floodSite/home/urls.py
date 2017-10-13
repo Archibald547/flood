57513d18681363b4df27eee1b046d033715e4015
Jenny <nzhe4831@uni.sydney.edu.au>
2017-10-13 16:19:53 +1100

    change to simple url in redux

--- floodSite/home/urls.py
+++ floodSite/home/urls.py
@@ -5,6 +5,6 @@ from home import views
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-    url(r'^accounts/', include('registration.backends.default.urls'), name="register"),
+    url(r'^accounts/', include('registration.backends.simple.urls'), name="register"),
     url(r'^privacy_policy/$', views.privacy_policy.as_view(), name="privacy_policy"),
 ]
