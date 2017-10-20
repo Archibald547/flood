24422558fd9941906cc48b5b6253d5d5ae53b2a5
Nachi Mudgil <nmud6539@uni.sydney.edu.au>
2017-10-20 13:28:34 +0000

    about.html created online with Bitbucket

---
+++ floodSite/home/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render , HttpResponse, HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+# from floodApp.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from django.http import JsonResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+class privacy_policy(TemplateView):
+    template_name = "privacy_policy.html"
+
+
+
+# class about(TemplateView):
+#     template_name = "about.html"
+#
+# class contact(TemplateView):
+#     template_name = "contact.html"
+
+
