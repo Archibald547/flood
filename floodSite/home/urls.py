95fe18f0c401ee2b6699779b6d9fc619b255128e
Jenny <nzhe4831@uni.sydney.edu.au>
2017-10-21 16:51:19 +1100

    created extra fields for register. extended user model. created profile. TODO: testing

--- floodSite/home/urls.py
+++ floodSite/home/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from home import views
+from home.regbackend import MyRegistrationView
 
 
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls'), name="register"),
     url(r'^privacy_policy/$', views.privacy_policy.as_view(), name="privacy_policy"),
 ]
