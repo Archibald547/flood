ff0b96c895b5c4d791e6abf4ce4930c6eeacb8ae
Jessica Huang <Jessica Huang@localhost>
2017-10-28 15:28:35 +1100

    completed tasks now contribute to exp

--- floodSite/home/urls.py
+++ floodSite/home/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^accounts/register/$', MyRegistrationView.as_view(),name='registration_register'),
     url(r'^register/closed/$', TemplateView.as_view(template_name='registration/registration_closed.html'),name='registration_disallowed'),
-    # url(r'^forum/', include(board.urls), name='forum'),
     url(r'^accounts/', include('registration.backends.simple.urls'), name="register"),
     url(r'^privacy_policy/$', views.privacy_policy.as_view(), name="privacy_policy"),
-    # url(r'^about/$', views.about.as_view(), name="about"),
-    # url(r'^contact/$', views.contact.as_view(), name="contact"),
 ]
