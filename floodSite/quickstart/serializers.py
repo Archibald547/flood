7f1978e83d8dd50cb9dd33bd5dab7b9fea9a5dd9
Archibald Weng <awen9183@uni.sydney.edu.au>
2017-10-28 15:27:19 +1100

    Added rest api for myprofile

--- floodSite/quickstart/serializers.py
+++ floodSite/quickstart/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from home.models import MyProfile
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MyProfile
+        fields = ('user','exp')
