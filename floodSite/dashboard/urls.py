16d1f29586b2bbb59826b0369c05e828f5d3a630
Jenny <nzhe4831@uni.sydney.edu.au>
2017-10-30 14:04:35 +1100

    fixed dashboard background

---
+++ floodSite/dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from dashboard import views
+
+urlpatterns = [
+    url(r'^dashboard/$', views.dashboard, name="dashboard"),
+    url(r'^profile/$', views.profile.as_view(), name="profile"),
+    url(r'^update_profile/$', views.update_profile, name="update_profile"),
+    url(r'^change_password/$', views.change_password, name="change_password"),
+    url(r'^change_password/done/$', views.change_password_done, name="change_password_done"),
+]
