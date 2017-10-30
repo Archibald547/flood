6d4d00b6f4ab23be4c2feb2567eafb660d9ce22c
Jessica Huang <Jessica Huang@localhost>
2017-10-30 13:56:08 +1100

    link the chat app to icon

--- floodSite/dashboard/static/js/dashboard.js
+++ floodSite/dashboard/static/js/dashboard.js
@@ -1,7 +1,6 @@
 var opened = "no"
 var minutes = 5;
 var seconds = 0;
-var mode = "stop";
 var interval;
 
 function startTimer(duration, display, mode) {
@@ -88,24 +87,3 @@ function showTimer() {
         x.style.display = "none";
     }
 }
-
-        // document.getElementById('timer').innerHTML = 03 + ":" + 00;
-        // startTimer();
-
-        // function startTimer() {
-        //   var presentTime = document.getElementById('timer').innerHTML;
-        //   var timeArray = presentTime.split(/[:]+/);
-        //   var m = timeArray[0];
-        //   var s = checkSecond((timeArray[1] - 1));
-        //   if(s==59){m=m-1}
-        //   //if(m<0){alert('timer completed')}
-          
-        //   document.getElementById('timer').innerHTML = m + ":" + s;
-        //   setTimeout(startTimer, 1000);
-        // }
-
-        // function checkSecond(sec) {
-        //   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-        //   if (sec < 0) {sec = "59"};
-        //   return sec;
-        // }
