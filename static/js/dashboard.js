7e0bdcfb80667c018ded37dfaa53c910797d20f0
Jessica Huang <Jessica Huang@localhost>
2017-10-30 17:36:24 +1100

    commit

---
+++ static/js/dashboard.js
@@ -0,0 +1,65 @@
+var opened = "no"
+var timer = new Timer();
+
+timer.start({countdown: true, startValues: {seconds: 30}});
+$('#timer .values').html(timer.getTimeValues().toString());
+
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#timer .values').html(timer.getTimeValues().toString());
+});
+
+timer.addEventListener('targetAchieved', function (e) {
+    $('#timer .values').html('TIME\'S UP!!');
+});
+
+$(document).ready(function() {
+    window.location.href;
+    if (((window.location.href).indexOf("todo/task") !== -1) || 
+    ((window.location.href).indexOf("todo/complete") !== -1) || 
+    ((window.location.href).indexOf("todo/delete") !== -1) || 
+    ((window.location.href).indexOf("todo/list") !== -1)) {
+    	document.getElementById("dropup").className = "dropup open"    
+    }
+});
+
+function displayModal() {
+    if (opened == "no") {
+        document.getElementById("dropup").className = "dropup"
+        opened = "yes"
+        console.log("opened")
+    } else {
+        document.getElementById("dropup").className = "dropup open"
+        opened = "no"
+        console.log("closed")
+    }
+}
+
+function showTimer() {
+    var x = document.getElementById("timer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+        // document.getElementById('timer').innerHTML = 03 + ":" + 00;
+        // startTimer();
+
+        // function startTimer() {
+        //   var presentTime = document.getElementById('timer').innerHTML;
+        //   var timeArray = presentTime.split(/[:]+/);
+        //   var m = timeArray[0];
+        //   var s = checkSecond((timeArray[1] - 1));
+        //   if(s==59){m=m-1}
+        //   //if(m<0){alert('timer completed')}
+          
+        //   document.getElementById('timer').innerHTML = m + ":" + s;
+        //   setTimeout(startTimer, 1000);
+        // }
+
+        // function checkSecond(sec) {
+        //   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+        //   if (sec < 0) {sec = "59"};
+        //   return sec;
+        // }
