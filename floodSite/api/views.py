2f1585c096ae5222dc1d0a865ca81c2fc608a690
Jessica Huang <Jessica Huang@localhost>
2017-10-31 00:02:36 +1100

    commit

---
+++ floodSite/api/views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from api.serializers import UserSerializer, GroupSerializer,ProfileSerializer,TodoSerializer
+from home.models import MyProfile
+from myTodo.models import todo
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class MyProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user profiles to be viewed or edited.
+    """
+    queryset = MyProfile.objects.all()
+    serializer_class = ProfileSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user todo tasks to be viewed or edited.
+    """
+    queryset = todo.objects.all()
+    serializer_class = TodoSerializer
---
+++ floodSite/home/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url, include
+from home import views
+from home.regbackend import MyRegistrationView
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from django.core.urlresolvers import reverse_lazy
+from machina.app import board
+
+
+urlpatterns = [
+    url(r'^$', views.home, name="home"),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),name='registration_register'),
+    url(r'^register/closed/$', TemplateView.as_view(template_name='registration/registration_closed.html'),name='registration_disallowed'),
+    url(r'^accounts/', include('registration.backends.simple.urls'), name="register"),
+    url(r'^privacy_policy/$', views.privacy_policy.as_view(), name="privacy_policy"),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('home:password_reset_done'), template_name='registration/password_reset_form.html'),name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html',success_url=reverse_lazy('home:password_reset_complete')), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+
+]
