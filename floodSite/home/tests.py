aea9bb0a6c1f400206d5dde44bdcd1e236728582
Jessica Huang <Jessica Huang@localhost>
2017-11-03 00:42:13 +1100

    add some more test functions to todo tests

--- floodSite/home/tests.py
+++ floodSite/home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class HomeTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='jacob', password='top_secret')
+
+    def test_redirect_to_dashboard(self):
+        """Check if the redirection works"""
+        response = self.client.get('/dashboard', follow=True)
+        self.assertRedirects(response, '/chatroom')
+
+        response = self.client.get('/chatroom')
+        self.assertEqual(response.status_code, 301)
