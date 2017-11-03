052b8c4c1588d3441b981f23b920ed27a3bc9ab3
Jessica Huang <Jessica Huang@localhost>
2017-11-03 12:41:23 +1100

    home test cases

--- floodSite/home/tests.py
+++ floodSite/home/tests.py
@@ -1,14 +1,18 @@
+from django.urls import resolve
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.http import HttpRequest
+from .views import home 
 
-class HomeTestCase(TestCase):
-    def setUp(self):
-        User.objects.create_user(username='jacob', password='top_secret')
+class HomePageTest(TestCase):
 
-    def test_redirect_to_dashboard(self):
-        """Check if the redirection works"""
-        response = self.client.get('/dashboard', follow=True)
-        self.assertRedirects(response, '/chatroom')
+    #tests that the root url directs the user to the correct view in home.views
+    def test_root_url_directs_to_home_page_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, home)
 
-        response = self.client.get('/chatroom')
-        self.assertEqual(response.status_code, 301)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = home(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))  
+        self.assertIn('<title>Before The Flood</title>', html);
